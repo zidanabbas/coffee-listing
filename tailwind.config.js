b8c50a46a3143dd7661a5d20ef6fd58babc64bd8
zidanabbas <zidan.abbas28@gmail.com>
2024-01-24 16:07:41 +0700

    tailwindcss configuration theme

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,8 +1,20 @@
 /** @type {import('tailwindcss').Config} */
 export default {
-  content: ["./src/**/*.{js,jsx,ts,tsx}"],
+  darkMode: "class",
+  content: ["./index.html", "./src/**/*.{js,ts,jsx,tsx}"],
   theme: {
-    extend: {},
+    screens: {
+      dekstop: "1280px",
+      tablet: "1024px",
+      mobile: "390px",
+    },
+    extend: {
+      colors: {
+        primary: "#111315",
+        secondary: "#1B1D1F",
+        third: "#6F757C",
+      },
+    },
   },
   plugins: [],
 };
