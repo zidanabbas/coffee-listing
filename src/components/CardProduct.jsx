054de1efdf5266f25cf087ad66ed8484ce2bf96e
zidanabbas <zidan.abbas28@gmail.com>
2024-01-25 23:39:58 +0700

    updated responsive coffee list in Card Product.jsx

--- src/components/CardProduct.jsx
+++ src/components/CardProduct.jsx
@@ -1,21 +1,25 @@
 import React from "react";
 
 const CardProduct = (props) => {
-  const { image, name, price, votes } = props;
+  const { image, name, price, rating, votes } = props;
   return (
-    <article className="container max-w-60 rounded-xl shadow-2xl overflow-hidden bg-slate-700">
+    <article className="container max-w-60 min-h-48 rounded-xl shadow-2xl overflow-hidden">
       <figure className="relative w-full object-contain">
         <img src={image} alt="" className="hover:animate-pulse" />
         <span className="absolute flex items-center bg-yellow-500 top-2 mx-2 rounded-full px-2 ">
           <p className="text-sm">Popular</p>
         </span>
       </figure>
-      <figcaption className="flex justify-between text-lg text-white px-2">
-        <p className="text-lg font-semibold">{name}</p>
-        <span>{price}</span>
+      <figcaption className="flex my-1 justify-between items-center text-white px-2">
+        <p className="font-DM-sans text-sm font-light">{name}</p>
+        <span className="font-DM-sans text-xs bg-fourth text-primary px-1 rounded-sm">
+          {price}
+        </span>
       </figcaption>
-      <span className="inline-flex gap-2 px-2">
-        ⭐ <p className="text-white">4.7 ({votes})</p>
+      <span className="inline-flex items-center gap-2 px-2">
+        <p className="text-white font-DM-sans text-sm">
+          ⭐ {rating} ({votes})
+        </p>
       </span>
     </article>
   );
